b72534e8006c1ed052e7801ffca94a2eeca73816
maheen-raza <113572260+maheen-raza@users.noreply.github.com>
2024-02-19 21:14:36 -0700

    started working on the frontend

---
+++ yeschef/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="mainpage">
+      <div className="div">
+        <div className="group">
+          <div className="overlap-group">
+            <div className="rectangle">
+              <div className="text-wrapper">Log in</div>
+            </div>
+          </div>
+          <div className="group-2">
+            <div className="text-wrapper-2">Find Unique Recipes Fast</div>
+            <div className="text-wrapper-3">YES, CHEF</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
