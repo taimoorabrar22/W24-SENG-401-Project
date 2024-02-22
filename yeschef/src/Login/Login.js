fd43a56402328e774b31033b151cbd8b7664b80d
maheen-raza <113572260+maheen-raza@users.noreply.github.com>
2024-02-21 21:17:13 -0700

    working on the login page

---
+++ yeschef/src/Login/Login.js
@@ -0,0 +1,50 @@
+import "./Login.css"
+
+function Login() {
+    return (
+        <div className="login">
+            <div className="div">
+                <div className="group">
+                    <div className="overlap">
+                        <div className="text-wrapper">PASSWORD</div>
+                        <div className="overlap-group">
+                            <img
+                                className="rectangle"
+                                alt="Rectangle"
+                                src="https://cdn.animaapp/projects/65d41c71641b9dafb8dc247e/releases/65d41cc6d92080865e1e8a25/img/rectangle-5-1.svg"
+                            ></img>
+                            <div className="create-account">OR CREATE AN ACCOUNT</div>
+                        </div>
+                        <div  className="text-wrapper-2">LOGIN</div>
+                        <img
+                            className="img"
+                            alt="Rectangle"
+                            src="https://cdn.animaapp/projects/65d41c71641b9dafb8dc247e/releases/65d41cc6d92080865e1e8a25/img/rectangle-5-1.svg"
+                        />
+                        <div className="overlap-group-warpper">
+                            <div className="overlap-group-2">
+                                <div className="rectangle-2">
+                                    <div className="text-wrapper-3">Log In</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="text-wrapper-4">USERNAME</div>
+                        <img
+                            className="rectangle-3"
+                            alt="Rectangle"
+                            src="https://cdn.animaapp/projects/65d41c71641b9dafb8dc247e/releases/65d41cc6d92080865e1e8a25/img/rectangle-5-1.svg"
+                        />
+                    </div>
+                </div>
+                <div className="overlap-wrapper">
+                    <div className="overlap-2">
+                        <div className="text-wrapper-5">YES,</div>
+                        <div className="text-wrapper-6">CHEF</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
--- yeschef/src/index.js
+++ yeschef/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './WebHome/App';
+import Login from './Login/Login';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Login />
   </React.StrictMode>
 );
 
