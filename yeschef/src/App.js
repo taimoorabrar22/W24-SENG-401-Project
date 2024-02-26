b51e74b4cb7a4cdbfbc663224f9358a907533b25
mzafari2 <113462468+mzafari2@users.noreply.github.com>
2024-02-25 23:57:33 -0700

    added Router for login

--- yeschef/src/App.js
+++ yeschef/src/App.js
@@ -16,3 +16,22 @@ const App = () => {
 export default App;
 
 */
+
+// App.js or your main routing component
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import WebHome from "./WebHome/WebHome";
+import Login from "./Login/Login";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Route path="/" exact component={WebHome} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
--- yeschef/src/index.js
+++ yeschef/src/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-//import App from './App';
-import reportWebVitals from './reportWebVitals';
-import WebHome from './WebHome/WebHome';
-import Login from './Login/Login';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import WebHome from "./WebHome/WebHome";
+import Login from "./Login/Login";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals"; // Add this line
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <WebHome />
-    <Login />
+    <Router>
+      <Routes>
+        {" "}
+        {/* Use Routes instead of div */}
+        <Route path="/" element={<WebHome />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
